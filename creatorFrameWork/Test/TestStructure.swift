7f08086b4bb95b4aedca9f3a11193bdadb072f76
Arun Godumala <arun.godumala@qumu.com>
2016-04-28 16:31:59 +0530

    aaa

---
+++ creatorFrameWork/Test/TestStructure.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  creatorFrameWork
+//
+//  Created by Arun Godumala on 27/04/16.
+//  Copyright © 2016 Arun. All rights reserved.
+//
+
+import Foundation
+
+
+public class TestStructure{
+
+    var name:String = ""
+    
+    var age:Int = 28
+    
+    
+    
+}
